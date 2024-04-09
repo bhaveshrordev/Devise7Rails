e2d59d52fa85d015a35ac2806d165d08846cbd80
bhaveshrordev <salujabhavesh24@gmail.com>
2024-04-09 15:17:45 +0530

    install devise

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem 'devise', github: 'heartcombo/devise', branch: 'main'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static_pages/landing_page'
   get 'static_pages/dashboard'
+
+  root to: "static_pages#landing_page"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
