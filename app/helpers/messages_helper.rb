9b800c1ecf2c563beaf59de0e02552fbf59c5825
bhaveshrordev <salujabhavesh24@gmail.com>
2024-04-13 19:16:38 +0530

    added label method in message helper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,26 @@
 module MessagesHelper
+
+  def upvote_label(message, user)
+    label_text =  if user.voted_up_on? message
+                    "UNvote"
+                  else
+                    "UPvote"
+                  end
+
+    tag.span do
+      "#{message.cached_votes_up} #{label_text}"
+    end
+  end
+
+  def downvote_label(message, user)
+    label_text =  if user.voted_down_on? message
+                    "UNvote"
+                  else
+                    "DOWNvote"
+                  end
+    tag.span do
+      "#{message.cached_votes_down} #{label_text}"
+    end
+  end
+
 end
