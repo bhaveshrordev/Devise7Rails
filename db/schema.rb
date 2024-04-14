840b3030954b76529c17af2915531bdce5dae1e6
bhaveshrordev <salujabhavesh24@gmail.com>
2024-04-14 20:43:25 +0530

    removed cached columns from messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_12_124307) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_14_151030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,13 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_12_124307) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "cached_votes_total", default: 0
-    t.integer "cached_votes_score", default: 0
-    t.integer "cached_votes_up", default: 0
-    t.integer "cached_votes_down", default: 0
-    t.integer "cached_weighted_score", default: 0
-    t.integer "cached_weighted_total", default: 0
-    t.float "cached_weighted_average", default: 0.0
   end
 
   create_table "users", force: :cascade do |t|
